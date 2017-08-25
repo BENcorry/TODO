c971e4a166afd021d6a64d3854416548d8b92b6e
BENcorry <1417224785@qq.com>
2017-08-25 15:58:28 +0800

    m vim vim
    å®æå¢å åè½a

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,8 @@
   <div class="item">
      <h3>{{list.name}}</h3>
     <p>{{list.content}}</p>
+    <span class="delete" @click.prevent="closeList">×</span>
+    <button @click.prevent="deleteList">删除</button>
   </div>
   
   
@@ -16,6 +18,18 @@ export default {
         list:{}
       }
     },
+    methods : {
+      deleteList : function(){
+        this.$http.delete('https://my-first-vue.firebaseio.com/add/' + this.id + ".json")
+        .then((data) => {
+          console.log(data)
+          this.$router.push({path:'/'})
+        })
+      },
+      closeList : function(){
+        this.$router.push({path:'/'})
+      }
+    },
     created(){
       this.$http.get('https://my-first-vue.firebaseio.com/add/' + this.id + ".json")
         .then(function(data){
@@ -24,10 +38,12 @@ export default {
           // this.blog = data.body;
         })
         .then(function(data){
-          this.list = data;
+          this.list = data
+          console.log(this.id)
           console.log(data.name)
         })
     }
+    
   }
   
 </script>
@@ -35,12 +51,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .item{
+  position: relative;
   margin: 200px auto;
   max-width: 800px;
   min-height: 300px;
   background-color: #f6f6f6;
   padding: 5px 0;
   border-radius: 10px;
+}
+.delete{
+  position: absolute;
+  display: block;
+  top: 10px;
+  right: 20px;
+  cursor: pointer;
+}
+.delete:hover{
+  background-color: rgba(0,0,0,.3);
 }
 h3{
   text-align: center;
@@ -49,5 +76,22 @@ h3{
 p{
   padding: 10px;
   text-indent: 2em;
+}
+button{
+  display: block;
+  position: absolute;
+  width: 60px;
+  height: 30px;
+  bottom: 10px;
+  left: 50%;
+  margin-left: -30px;
+  border : 0;
+  background-color: #D06B66;
+  border-radius: 4px;
+  font-size: 14px;
+  color: #fff;
+}
+button:hover{
+  background-color: red;
 }
 </style>
